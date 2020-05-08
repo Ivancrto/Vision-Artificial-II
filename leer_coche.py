2faf1164ef697c02311806a753899fbf86386046
Ivancrto <ivancrtoc@gmail.com>
2020-05-08 20:47:38 +0200

    last version

--- leer_coche.py
+++ leer_coche.py
@@ -5,83 +5,124 @@ Created on Sat Apr 25 14:46:54 2020
 @author: Ivanxrto
 """
 
-import numpy as np
-import cv2 
+import cv2
 from os import listdir
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+import numpy as np
+
 
 imagenes = []
 
 matricula_cascade = cv2.CascadeClassifier('./haar/matriculas.xml')
 frontal_cascade = cv2.CascadeClassifier('./haar/coches.xml')
+
+
 def comprobarmatricula(date_image):
+
+
     for i in date_image:
         im = i[0]
-        imc =i[1]
-        
-        imagenP = matricula_cascade.detectMultiScale (im, 1.1, 11) # 1parametro imagen
-        if imagenP is not():
-            # Por cada matricula detectada, dibujamos un rectangulo
+        imc = i[1]
+
+        imagenP = matricula_cascade.detectMultiScale(im, 1.1, 11)  # 1parametro imagen
+        #  if imagenP is not():
+        # Por cada matricula detectada, dibujamos un rectangulo
+        if imagenP is not ():
             for (x, y, w, h) in imagenP:
                 # Recorto la matricula
-                imagenrecortadagris =im[y: (y + h), x: (x + w)]
+                imagenrecortadagris = im[y: (y + h), x: (x + w)]
                 imagenrecortadanormal = imc[y: (y + h), x: (x + w)]
-                #https://docs.opencv.org/3.4/d7/d4d/tutorial_py_thresholding.html
-                
+                # https://docs.opencv.org/3.4/d7/d4d/tutorial_py_thresholding.html
+
                 ret, th3 = cv2.threshold(imagenrecortadagris, 0, 255, cv2.THRESH_OTSU)
-                #https://www.programcreek.com/python/example/89437/cv2.boundingRect
-                contours, hierarchy = cv2.findContours(th3, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-                #https://stackoverflow.com/questions/46971769/how-to-extract-only-characters-from-image
+                # https://www.programcreek.com/python/example/89437/cv2.boundingRect
+                contours, hierarchy = cv2.findContours(th3, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                # https://stackoverflow.com/questions/46971769/how-to-extract-only-characters-from-image
                 sorted_ctrs = sorted(contours, key=lambda ctr: cv2.boundingRect(ctr)[0])
-                #contours, hierarchy = cv2.findContours(th3,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+                # contours, hierarchy = cv2.findContours(th3,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+                for cor in sorted_ctrs:
+
+                    # Sacamos coordenadas de contorno
+                    a, b, c, d = cv2.boundingRect(cor)
+
+                    if d > c and c > 3 and d > 10:
+                        cv2.rectangle(imagenrecortadanormal, (a, b), (a + c, b + d), (200, 105, 0), 2)
+
+                cv2.imshow("prueba", imc)
+                cv2.waitKey()
+        else:
+            contorno = []
+            imagenPP = frontal_cascade.detectMultiScale(im, 1.1, 11)  # 1parametro imagen
+
+            for (x, y, w, h) in imagenPP:
+                data_x = np.array([])
+                data_y = np.array([])
+                recortar = im[y: (y + h), x: (x + w)]
+                recortarNormal = imc[y: (y + h), x: (x + w)]
+                th3 = cv2.adaptiveThreshold(recortar, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
+                                            cv2.cv2.THRESH_BINARY_INV, 19, 2)
+                # cv2.THRESH_BINARY   o cv2.THRESH_BINARY_INV
+                cv2.imshow("pruebath", th3)
+                cv2.waitKey()
+                # https://www.programcreek.com/python/example/89437/cv2.boundingRect
+                contours, hierarchy = cv2.findContours(th3, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+                # https://stackoverflow.com/questions/46971769/how-to-extract-only-characters-from-image
+                sorted_ctrs = sorted(contours, key=lambda ctr: cv2.boundingRect(ctr)[0])
+
+                # contours, hierarchy = cv2.findContours(th3,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
                 for cor in sorted_ctrs:
                     # get the bounding rect
                     a, b, c, d = cv2.boundingRect(cor)
-                    if d>c and c>5 and d>10:
-                        print(str(a) + " " + str(b) + " " + str(c)+ " " +str(d))        
-                        cv2.rectangle(imagenrecortadanormal, (a, b), (a+c, b+d), (200, 105, 0), 2)
-                    
-                cv2.imshow("prueba",imc)
-                cv2.waitKey()
-                print("--------------------------")
-        else:
-        
+                    anchoMin = 4
+                    anchoMax = 7
+                    if d > c and c > 3 and d > 10:
+                        data_x = np.append(data_x, float(a))
+                        data_y = np.append(data_y, float(b))
+                        contorno.append([a, b, c, d])
+                        cv2.rectangle(recortarNormal, (a, b), (a + c, b + d), (200, 105, 0), 2)
 
-            th3 = cv2.adaptiveThreshold(im,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-                                        cv2.cv2.THRESH_BINARY_INV,19,2)
-            #cv2.THRESH_BINARY   o cv2.THRESH_BINARY_INV
-            cv2.imshow("pruebath",th3)
+            if data_y.shape[0] != 0:
+                data_x = data_x.reshape((data_x.shape[0], 1))
+                ransac = linear_model.RANSACRegressor()
+                ransac.fit(data_x, data_y)
+
+                plt.scatter(data_x, data_y, color='black')
+                x0 = data_x[0]
+                x1 = data_x[data_x.shape[0] - 1]
+                y0 = ransac.estimator_.intercept_
+                y1 = x1 * ransac.estimator_.coef_ + ransac.estimator_.intercept_
+                plt.plot([x0, x1], [y0, y1], color='blue', linewidth=2)
+                plt.show()
+                m = ransac.estimator_.coef_
+                n = ransac.estimator_.intercept_
+                # print(str(ransac.estimator_.coef_) + " " + str(ransac.estimator_.intercept_))
+                for i in contorno:
+                    if (i[0] * m) + n + 2 > i[1] and (i[0] * m) + n - 2 < i[1]:
+                        print("---" + str((i[0] * m) + n) + " " + str(i[1]) + " " + str(i[2]) + " " + str(i[3]))
+
+                        cv2.rectangle(recortarNormal, (i[0], i[1]), (i[0] + i[2], i[1] + i[3]), (0, 105, 0), 2)
+
+            cv2.imshow("prueba", imc)
             cv2.waitKey()
-            #https://www.programcreek.com/python/example/89437/cv2.boundingRect
-            contours, hierarchy = cv2.findContours(th3, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-            
-            #https://stackoverflow.com/questions/46971769/how-to-extract-only-characters-from-image
-            sorted_ctrs = sorted(contours, key=lambda ctr: cv2.boundingRect(ctr)[0])
-            
-            #contours, hierarchy = cv2.findContours(th3,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-            for cor in sorted_ctrs:
-                # get the bounding rect
-                a, b, c, d = cv2.boundingRect(cor)
-                if d>c and c>5 and d>5 and c<40 and d<40:
-                    print(str(a) + " " + str(b) + " " + str(c)+ " " +str(d))
-                    cv2.rectangle(imc, (a, b), (a+c, b+d), (200, 105, 0), 2)
-                    
-            cv2.imshow("prueba",imc)
-            cv2.waitKey()
-            
+
+
 
 def cargar_imagen(date_image):
     for img in listdir("./testing_full_system"):
-         #1.1 La carga debería realizarse en niveles de gris. Esto se consigue poniendo a 0 el segundo
-             #parámetro del comando cv2.imread. 
-         img_grey = cv2.imread("./testing_full_system/" + img, 0)
-         img_color = cv2.imread("./testing_full_system/" + img)
-         date_image.append([img_grey, img_color])
-    
+        # 1.1 La carga debería realizarse en niveles de gris. Esto se consigue poniendo a 0 el segundo
+        # parámetro del comando cv2.imread.
+        img_grey = cv2.imread("./testing_full_system/" + img, 0)
+        img_color = cv2.imread("./testing_full_system/" + img)
+        date_image.append([img_grey, img_color])
+
+
 def main():
     cargar_imagen(imagenes)
     comprobarmatricula(imagenes)
-   
-    
-    
+
+
 if __name__ == "__main__":
     main()
